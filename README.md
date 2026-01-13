8b136b6ef6b4fa2868ba8ad32df2655097996f41
TheRedQueen <pand3m1x@gmail.com>
2026-01-13 14:11:49 -0500

    ⋆˙⟡Fixed HTML code⋆˙⟡

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 READ ME Reflection for module 4 lab 4.2
 
 How did array methods like push() and filter() help you manipulate data in this activity?
-	a:
+	a: Honestly, I am still confused. My classmates were very helpful in the item list activity, and while there was a lot of conversations, I was having computer problems those days, so it was difficult to follow. So I've spent a lot of time watching Youtube videos to explain thing.
 
 What challenges did you face when trying to filter and search for items in the array?
-	a:
+	a: Everything. I think figuring out what the HTML code needs to make the javascript work was difficult to understand. I had issues with ordering things correctly so the wrong thing wasn't taking over something else. 
 
 How would you modify this code to make it more efficient or user-friendly?
-	a:
+	a: I guess this is where CSS would come in? We didn't set it up so 'enter' key doesn't work with input, which is useful for people who use keyboards. A print/mail list option? So once the list was completed the user could have a copy of the list. It could be optimized for mobile devices. And maybe a priority option, or a way to order things by catergory, for people shopping at multiple locations. 
--- script.js
+++ script.js
@@ -4,4 +4,69 @@ shopping list with:
 
 1) Add Item
 2) Remove Last Item
-3) Display List */
+3) Display List 
+
+I am still confused on Const versus Let, it feels like I could make clearer
+code by*/
+
+let cart = [];
+
+let itemInput = document.getElementById("itemInput");
+
+let addItemButton = document.getElementById("addItemButton");
+
+let removeItemButton = document.getElementById("removeItemButton");
+
+let cartList = document.getElementById("cart");
+
+addItemButton.addEventListener("click", function () {
+  let item = itemInput.value;
+
+  if (item === "") {
+    alert("Please enter an item.");
+
+    return;
+  }
+
+  cart.push(item); // Add item to cart array
+
+  renderCart();
+
+  itemInput.value = ""; // Clear the input field
+});
+
+removeItemButton.addEventListener("click", function () {
+  cart.pop(); // Remove last item from cart array
+
+  renderCart();
+});
+
+function renderCart() {
+  cartList.innerHTML = ""; // Clear existing list
+
+  for (let i = 0; i < cart.length; i++) {
+    let listItem = document.createElement("li");
+
+    listItem.innerText = cart[i];
+
+    cartList.appendChild(listItem);
+
+    let deleteBtn = document.createElement("button");
+
+    deleteBtn.innerText = "remove";
+
+    listItem.appendChild(deleteBtn);
+
+    deleteBtn.classList.add("removeBtn");
+
+    deleteBtn.setAttribute("data-item-index", i);
+
+    deleteBtn.addEventListener("click", function () {
+      let index = this.getAttribute("data-item-index");
+
+      cart.splice(index, 1);
+
+      renderCart();
+    });
+  }
+}
